4551375f95dfe5e073ee1e80192f07fe5a418fe2
Ambalika Singh <ambsingh@adobe.com>
2024-07-30 16:51:49 +0530

    initial commit

---
+++ libs/features/spectrum-web-components/src/styles.js
@@ -0,0 +1,9 @@
+import '@spectrum-web-components/styles/all-medium-light.css'
+import '@spectrum-web-components/styles/typography.js'
+import '@spectrum-web-components/styles/body.js'
+import '@spectrum-web-components/styles/code.js'
+import '@spectrum-web-components/styles/detail.js'
+import '@spectrum-web-components/styles/heading.js'
+
+export * from '@spectrum-web-components/card';
+export { n as P} from `@spectrum-web-components/card`;
---
+++ libs/views/select-theme/select-theme.css.js
@@ -0,0 +1,52 @@
+import { css } from "../../deps/lit-all.min.js";
+
+export const style = css`
+ 
+    #select-theme-container {
+        display: flex;
+        flex-direction: column;
+        margin-block-start: 100px;
+        flex-direction: column;
+        flex-grow: auto;
+        gap: 30px;
+        margin-left: auto;
+        margin-right: auto;
+    }
+
+    h1 {
+        text-align : center;
+    }
+
+    h2{
+        text-align : center;
+    }
+
+    form {
+        display: flex;
+        flex-direction: column;
+        gap: 30px;
+    }
+
+    #cards-container {
+        display: flex;
+        flex-direction: row;
+        gap : 12px;
+    }
+
+    .card {
+       outline: 2px solid black;
+        border-radius: 5px;
+        border: 2px double grey;
+    }
+
+    .card:hover {
+        border : 2px solid blue;
+        outline: 2px solid black;
+    }
+
+    .selected {
+      border : 2px solid blue;
+       outline: 2px solid blue;
+    }
+
+`;
